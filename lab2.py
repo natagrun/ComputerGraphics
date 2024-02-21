170f8515310e0599a05131295e772e09b8e78642
Nata <ngrunskaya1994@gmail.com>
2024-02-21 19:22:41 +0300

    skibidi toilet

--- lab2.py
+++ lab2.py
@@ -1,20 +1,48 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
 from tkinter import Canvas
+from tkinter import messagebox
+
+
+class Point(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.coordinates = [0, 0]
+        self.confirm_button = tk.Button
+        self.number = ''
+        self.point_frame = tk.Frame(self, width=100, height=50)
+        self.point_frame.pack()
+        new_point_label = tk.Label(self.point_frame, text="Point " + self.number)
+        new_point_label.pack()
+
+        x_label = tk.Label(self.point_frame, text="X:")
+        x_label.pack()
+        self.x_entry = tk.Entry(self.point_frame)
+        self.x_entry.pack()
+
+        y_label = tk.Label(self.point_frame, text="Y:")
+        y_label.pack()
+        self.y_entry = tk.Entry(self.point_frame)
+        self.y_entry.pack()
+
+        self.confirm_button = tk.Button(self.point_frame, text="Confirm coordinates", command=self.confirm_coordinates)
+        self.confirm_button.pack(pady=20)
+
+    def confirm_coordinates(self):
+        self.coordinates[0] = int(self.x_entry.get())
+        self.coordinates[1] = int(self.y_entry.get())
 
 
 class BezierCurveApp(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.confirm_coords_button = None
+
         self.title("Bezier Curve App")
         self.geometry("800x600")
 
         self.points = []
-        self.point_coords = []
+        self.point_coordinates = []
 
-        self.menu_frame = tk.Frame(self, width=200, height=600, bg="lightgrey")
+        self.menu_frame = tk.Frame(self, width=200, height=600)
         self.menu_frame.pack(side="left", fill="y")
 
         self.canvas = Canvas(self, width=600, height=600, bg="white")
@@ -27,57 +55,41 @@ class BezierCurveApp(tk.Tk):
         self.draw_curve_button.pack(side="bottom")
 
     def add_point(self):
-        new_point_label = tk.Label(self.menu_frame, text="Point " + str(len(self.points) + 1))
-        new_point_label.pack()
-
-        x_label = tk.Label(self.menu_frame, text="X:")
-        x_label.pack()
-        x_entry = tk.Entry(self.menu_frame)
-        x_entry.pack()
-
-        y_label = tk.Label(self.menu_frame, text="Y:")
-        y_label.pack()
-        y_entry = tk.Entry(self.menu_frame)
-        y_entry.pack()
-
-        self.point_coords.append((x_entry, y_entry))
-
-        self.confirm_coords_button = tk.Button(self.menu_frame, text="Confirm Coordinates", command=self.confirm_coords)
-
-        self.confirm_coords_button.pack(pady=20)
-
-    def confirm_coords(self):
-        for x_entry, y_entry in self.point_coords:
-            x = x_entry.get()
-            y = y_entry.get()
-            if not x or not y:
-                messagebox.showerror("Error", "All coordinates must be filled")
-                return
-            self.points.append((int(x), int(y)))
-
-            self.point_coords.clear()
-            self.confirm_coords_button.config()
+        print('buttoned')
+        new_point = Point(self.menu_frame)
+        self.points.append(new_point)
+        new_point.pack(side='top', fill="both", expand=False)
 
     def draw_curve(self):
+
         if len(self.points) < 2:
             messagebox.showerror("Error", "At least 2 points are required to draw the Bezier curve")
             return
-
+        self.canvas.delete('all')
         # Draw x and y axis
         self.canvas.create_line(50, 550, 750, 550, arrow=tk.LAST)
         self.canvas.create_line(50, 550, 50, 50, arrow=tk.LAST)
 
+        # Add labels for x and y
+        for i in range(1, 15):
+            self.canvas.create_line(50 + i * 50, 550, 50 + i * 50, 545)
+            self.canvas.create_text(50 + i * 50, 560, text=str(i))
+
+        for i in range(1, 11):
+            self.canvas.create_line(50, 550 - i * 50, 55, 550 - i * 50)
+            self.canvas.create_text(40, 550 - i * 50, text=str(i))
+
         # Scale points for coordinate system
-        scaled_points = [(50 + point[0] * 10, 550 - point[1] * 10) for point in self.points]
+        scaled_points = [(20 + point.coordinates[0] * 10, 550 - point.coordinates[1] * 10) for point in self.points]
 
         # Draw points
         for point in scaled_points:
             self.canvas.create_oval(point[0] - 5, point[1] - 5, point[0] + 5, point[1] + 5, fill="black")
             self.canvas.create_line(scaled_points, fill="red", width=3)
 
-
         # Draw Bezier curve
         self.canvas.create_line(scaled_points, smooth=True, fill="blue", width=3)
+
 
 if __name__ == "__main__":
     app = BezierCurveApp()
